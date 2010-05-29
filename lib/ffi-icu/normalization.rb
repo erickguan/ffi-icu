ae6c5b4d4925a8d5527764a1bcccb700cb63b932
Jari Bakken <jari.bakken@gmail.com>
2010-05-29 02:57:47 +0200

    More cleanup

--- lib/ffi-icu/normalization.rb
+++ lib/ffi-icu/normalization.rb
@@ -2,27 +2,28 @@ module ICU
   module Normalization
 
     def self.normalize(input, mode = :default)
-      input_length = ICU.ruby19? ? input.length : input.jlength
-      needed_length  = 0
-      result_length = 0
+      input_length  = input.unpack("U*").size
+      needed_length = out_length = options = 0
+      in_ptr        = UCharPointer.from_string(input)
+      out_ptr       = UCharPointer.new(out_length)
 
       retried = false
-      ptr     = nil
 
       begin
         Lib.check_error do |error|
-          needed_length = Lib.unorm_normalize(UCharPointer.from_string(input), input_length, mode, 0, ptr, result_length, error)
+          needed_length = Lib.unorm_normalize(in_ptr, input_length, mode, options, out_ptr, out_length, error)
         end
       rescue BufferOverflowError
-        raise if retried
-        ptr     = UCharPointer.from_string("\0" * needed_length)
-        result_length = needed_length + 1
+        raise BufferOverflowError, "needed: #{needed_length}" if retried
+
+        out_ptr       = out_ptr.resized_to needed_length
+        out_length    = needed_length + 1
 
         retried = true
         retry
       end
 
-      ptr.string if ptr
+      out_ptr.string
     end
 
   end # Normalization
--- lib/ffi-icu/uchar.rb
+++ lib/ffi-icu/uchar.rb
@@ -8,15 +8,19 @@ module ICU
       str   = str.encode("UTF-8") if str.respond_to? :encode
       bytes = str.unpack("U*")
 
-      ptr = new UCHAR_TYPE, bytes.size
+      ptr = new bytes.size
       ptr.put_array_of_uint16 0, bytes
 
       ptr
     end
 
+    def initialize(size)
+      super UCHAR_TYPE, size
+    end
+
     def resized_to(new_size)
       raise "new_size must be larger than current size" if new_size < size
-      resized = self.class.new UCHAR_TYPE, new_size
+      resized = self.class.new new_size
       resized.put_bytes(0, get_bytes(0, size))
 
       resized
