b98634d2cc095873da97b0b1f1d666684aca3673
Marek Veber <marek.veber@solnet.cz>
2015-07-08 18:21:19 +0200

    fix - compatibility with ruby 1.8

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,3 +11,15 @@ RSpec.configure do |config|
     config.filter_run_excluding :broken => true
   end
 end
+
+if RUBY_VERSION < '1.9'
+require 'iconv'
+class String
+    def encode(charset)
+	Iconv.iconv('UTF-8', charset, self)
+    end
+    def force_encoding(charset)
+	self
+    end
+end
+end
