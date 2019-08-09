bb843660e82724d6518b5992d95f33905d6ebcc7
Damian Nelson <dnelson@zendesk.com>
2019-08-09 12:58:44 -0700

    Update tests for new CLDR data

--- spec/locale_spec.rb
+++ spec/locale_spec.rb
@@ -111,7 +111,7 @@ module ICU
 
         it 'returns the script' do
           Locale.new('ja_Hira_JP').display_script('en').should == 'Hiragana'
-          Locale.new('ja_Hira_JP').display_script('ru').should == 'Хирагана'
+          Locale.new('ja_Hira_JP').display_script('ru').should == 'хирагана'
         end
 
         it 'returns the variant' do
